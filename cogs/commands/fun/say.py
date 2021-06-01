e068ebe9c2568b20bbbb7ff86bf33d2da2bf841e
very-unique-username <dgm82382@gmail.com>
2021-06-02 00:34:47 +0530

    Initial commit

---
+++ cogs/commands/fun/say.py
@@ -0,0 +1,29 @@
+"""
+Got help on this one from Black Tooth
+https://blacktooth-bot.com
+"""
+import discord
+
+from discord.ext import commands
+
+class Say(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command(name = 'say', help = 'Make the bot say stuff')
+    async def say(self, ctx, *, message):
+        if 'https://' in message or 'http://' in message:
+            await ctx.send("I'm afraid I can't send links.")
+            return
+        await ctx.message.delete()
+        await ctx.send(f"{ctx.author.mention} said {message}",
+                       allowed_mentions = discord.AllowedMentions(everyone = False,
+                                                                  roles = False,
+                                                                  users= False
+                                                                 )
+                      )
+
+
+def setup(client):
+    client.add_cog(Say(client))
+    print('Say')
