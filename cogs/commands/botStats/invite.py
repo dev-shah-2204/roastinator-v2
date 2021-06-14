c8a077c512c81d8e6b4074cc41b0627820388878
very-unique-username <dgm82382@gmail.com>
2021-06-14 15:05:14 +0530

    Change invite link

--- cogs/commands/botStats/invite.py
+++ cogs/commands/botStats/invite.py
@@ -6,11 +6,11 @@ class Invite(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command(name = 'invite')
+    @commands.command(name = 'invite', aliases = ['addbot'])
     async def invite(self, ctx):
         em = discord.Embed(
             title = "Thank you for inviting me",
-            value = "Click [here](https://discord.com/api/oauth2/authorize?client_id=822795444089782293&permissions=1879436374&scope=bot)"
+            value = "Click [here](https://discord.com/api/oauth2/authorize?client_id=822795444089782293&permissions=1879436375&scope=bot)"
         )
         em.set_thumbnail(url = self.client.user.avatar_url)
         em.set_footer(text = 'Some of the required permissions might not be required, but they are for future updates')
