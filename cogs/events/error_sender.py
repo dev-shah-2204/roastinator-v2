31fb397bb4aa848ec5b386d8748440c62323c717
dev-shah-2204 <dgm82382@gmail.com>
2021-06-24 16:32:39 +0530

    Send error to bot owner

--- cogs/events/error_sender.py
+++ cogs/events/error_sender.py
@@ -1,4 +1,5 @@
 import discord
+import hex_colors
 from discord.ext import commands
 
 
@@ -8,7 +9,20 @@ class ErrorSender(commands.Cog):
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
+        await ctx.send("An error occured that I wasn't able to handle myself. This has been conveyed to my developer.")
         await ctx.send(f"```{error}```")
+
+        owner = self.client.get_user(416979084099321866) #Enter your ID here
+        em = discord.Embed(title = 'Error', color = hex_colors.m_red)
+        em.add_field(name = 'Command', value = ctx.command, inline = False)
+        em.add_field(name = 'Error:', value = f"```{error}```", inline = False)
+        em.add_field(name = 'Server:', value = f"{ctx.guild} ({ctx.guild.id})", inline = False)
+        em.add_field(name = 'User:', value = f"{ctx.author} ({ctx.author.id})", inline = False)
+        em.add_field(name = 'Message:', value = ctx.message)
+
+        await owner.send(embed = em)
+        
+        #The embed is optional, I want to see it so that I can understand the problem and fix it. 
 
 def setup(client):
     client.add_cog(ErrorSender(client))
