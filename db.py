a399439b1345a12e65b067d8b6dcbd1476c9be74
dev-shah-2204 <dgm82382@gmail.com>
2021-06-26 19:35:36 +0530

    Initial commit

---
+++ db.py
@@ -0,0 +1,14 @@
+import mysql.connector
+import os
+
+db_host = os.environ.get('db_host')
+db_user = os.environ.get('db_user')
+db_password = os.environ.get('db_password')
+db_db = os.environ.get('db_db')
+
+database = mysql.connector.connect(
+    host = db_host, #Enter your host
+    user = db_user, #Enter your user
+    password = db_password, #Enter your password
+    database = db_db #Enter your database name
+)
