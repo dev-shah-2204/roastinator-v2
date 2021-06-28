41fab4928b3e9251353a941062eabc43302efe1a
dev-shah-2204 <dgm82382@gmail.com>
2021-06-28 12:21:22 +0530

    Add state

--- cogs/commands/utility/starboard.py
+++ cogs/commands/utility/starboard.py
@@ -4,6 +4,7 @@ import hex_colors
 from db import *
 from discord.ext import commands
 
+db = database.cursor()
 class StarboardCommands(commands.Cog):
     def __init__(self, client):
         self.client = client 
@@ -14,13 +15,37 @@ class StarboardCommands(commands.Cog):
         em = discord.Embed(
             title = "Starboard Commands",
             description = """
-`channel`
-`enable`
-`disable`
+`channel` - Set the starboard channel
+`enable`  - Enable starboard for the server
+`disable` - Disable starboard for the server
             """,
             color = hex_colors.l_yellow)
 
         await ctx.send(embed = em)
+
+    @starboard_commands.command(name = 'channel', help = "Set the starboard channel")
+    async def channel(self, ctx, channel:discord.TextChannel):
+        db.execute(f"INSERT INTO Starboard(guild, channel, state) VALUES ('{ctx.guild.id}','{channel}','enabled')")
+        database.commit()
+        await ctx.send(f"Starboard set to <#{channel.id}>")
+
+    @starboard_commands.command(name = 'disable', help = "Disable starboard")
+    async def disable(self, ctx):
+        try:
+            db.execute(f"UPDATE Starboard SET state = 'disabled' WHERE guild = '{ctx.guild}'")
+            database.commit()
+            await ctx.send("Disabled starboard")
+        except:
+            await ctx.send("Starboard was never enabled for this server")
+
+    @starboard_commands.command(name = 'enable', help = "Enable starboard")
+    async def enable(self, ctx):
+        try:
+            db.execute(f"UPDATE Starboard SET state = 'enabled' WHERE guild = '{ctx.guild}'")
+            database.commit()
+            await ctx.send("Enabled starboard for this server")
+        except:
+            await ctx.send("Starboard has not been set in this server. First run the `starboard channel` command")
 
 
 def setup(client):
--- cogs/events/starboard.py
+++ cogs/events/starboard.py
@@ -16,6 +16,13 @@ class StarboardEvent(commands.Cog):
         channel = await get_data(db = db)
         return channel
 
+    async def get_star_guild_state(self, guild):
+        db.execute(f"SELECT status FROM Starboard WHERE guild = '{guild}'")
+        state = await get_data(db = db)
+        return state
+
+    starcount = {}
+
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
         if payload.emoji.name == '⭐':
@@ -23,26 +30,42 @@ class StarboardEvent(commands.Cog):
             if channel is None:
                 return
 
-            msg_channel = await self.client.get_channel(payload.channel_id)
-            msg = await msg_channel.fetch_message(payload.message_id)            
-            user = await self.client.get_user(payload.user_id)
+            state = await self.get_star_guild_state(payload.guild.id)
+            if state is 'enabled':
+                msg_channel = await self.client.get_channel(payload.channel_id)
+                msg = await msg_channel.fetch_message(payload.message_id)            
+                user = await self.client.get_user(payload.user_id)
 
-            if not payload.member.guild_permissions.manage_messages: #If the member doesn't have manage_messages permission
-                await msg_channel.send(f"{user.mention}, You need `Manage Messages` permission to star messages")
-                return 
+                if not payload.member.guild_permissions.manage_messages: #If the member doesn't have manage_messages permission
+                    await msg_channel.send(f"{user.mention}, You need `Manage Messages` permission to star messages")
+                    return 
 
-            em = discord.Embed(
-                description = f"[Jump to message]({msg.jump_url})", #jump_url is the message link
-                color = hex_colors.l_yellow,
-                timestamp = msg.created_at 
-                )
-            em.set_author(name = f"{user.name} starred a message", icon_url = user.avatar_url)
+                em = discord.Embed(
+                    description = f"[Jump to message]({msg.jump_url})", #jump_url is the message link
+                    color = hex_colors.l_yellow,
+                    timestamp = msg.created_at 
+                    )
+                em.set_author(name = f"{user.name} starred a message", icon_url = user.avatar_url)
 
-            if len(msg.attachments) > 0:
-                em.set_image(url = msg.attachments[0].url)
+                if len(msg.attachments) > 0:
+                    em.set_image(url = msg.attachments[0].url)
 
-            em.add_field(name = f"{msg.author} said:", value = msg.content)
-            await channel.send(embed = em)
+                em.add_field(name = f"{msg.author} said:", value = msg.content)
+                
+                starcount = self.starcount
+                
+                #Only the first reaction should star the message
+                if str(payload.message_id) in starcount:
+                    starcount[str(payload.message_id)] += 1
+
+                if not str(payload.message_id) in starcount:
+                    starcount[str(payload.message_id)] = 1
+
+                if starcount[str(payload.message_id)] <= 1:
+                    await channel.send(embed = em)
+
+                if starcount[str(payload.message_id)] > 1:
+                    return
 
         
 def setup(client):
