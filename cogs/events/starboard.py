e7eb684810c1c3f01a8dde62b6908284364750a8
dev-shah-2204 <dgm82382@gmail.com>
2021-06-29 11:44:31 +0530

    Dont buffer. buffer bad

--- cogs/events/starboard.py
+++ cogs/events/starboard.py
@@ -5,7 +5,7 @@ import hex_colors
 from db import *
 from discord.ext import commands 
 
-db = database.cursor(buffered = True)
+db = database.cursor()
 
 class StarboardEvent(commands.Cog):
     def __init__(self, client):
