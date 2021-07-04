b5a321b6b162a62f05a41bb8f3e79fcbadf65c99
dev-shah-2204 <dgm82382@gmail.com>
2021-07-04 19:34:11 +0530

    Send message on guild join

--- cogs/events/on_guild_join.py
+++ cogs/events/on_guild_join.py
@@ -12,6 +12,19 @@ class OnGuildJoin(commands.Cog):
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
+        for channel in guild.text_channels:
+            if guild.me.guild_permissions.send_messages:
+                em = discord.Embed(
+                    title='Hey there!',
+                    description='Thanks for inviting me to your server.\nMy prefix is `-`. If you wish to change it, use the prefix command.',
+                    color=0x60FF60
+                )
+                em.add_field(
+                    name='Example usage:',
+                    value=f'{self.client.user.mention} prefix `newPrefix`\nor\n-prefix `newPrefix`'
+                )
+                await channel.send(embed=em)
+                break
         db.execute(f"INSERT INTO Prefix(guild, prefix) VALUES ('{guild.id}','-')")
         db.execute(f"INSERT INTO AutoMod(guild, _status) VALUES ('{guild.id}','enabled')")
         database.commit()
