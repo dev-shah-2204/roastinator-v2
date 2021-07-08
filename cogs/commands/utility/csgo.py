dd2cec8ec36198542069ef6433399cc5cec6a7a9
dev-shah-2204 <dgm82382@gmail.com>
2021-07-08 17:04:44 +0530

    Re-format

--- cogs/commands/utility/csgo.py
+++ cogs/commands/utility/csgo.py
@@ -1,57 +1,57 @@
-import discord 
-import requests 
-import hex_colors 
-import asyncio 
-import os 
+import discord
+import requests
+import hex_colors
+import asyncio
+import os
 import random
 
 from discord.ext import commands
 
+
 class CSGOStats(commands.Cog):
     def __init__(self, client):
-        self.client = client 
+        self.client = client
 
     async def get_id_from_url(self, url):
-        url = url.replace('https://','')
-        url = url.replace('steamcommunity.com/profiles/','')
-        url = url.replace('steamcommunity.com/id/','')
-        url = url.strip('/')    
-        
+        url = url.replace('https://', '')
+        url = url.replace('steamcommunity.com/profiles/', '')
+        url = url.replace('steamcommunity.com/id/', '')
+        url = url.strip('/')
+
         try:
-            _id = int(url) #If the user doesn't have a vanity url
+            _id = int(url)  # If the user doesn't have a vanity url
             return _id
-        except:
-            _id = requests.get(f"http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key={os.environ.get('STEAM_API_KEY')}&vanityurl={url}").json() #To get ID from vanity URL
+        except ValueError:
+            _id = requests.get(f"http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key={os.environ.get('STEAM_API_KEY')}&vanityurl={url}").json()  # To get ID from vanity URL
 
         try:
             return _id['response']['steamid']
-        except:
+        except KeyError:
             return None
-
 
     @commands.command(name='csgo', aliases=['csgostats'], help="Get CSGO Stats of a Steam Account")
     async def get_csgo_stats(self, ctx, steam_profile_link=None):
-        def check(message:discord.Message) -> bool:
-            return message.author == ctx.author 
+        def check(message: discord.Message) -> bool:
+            return message.author == ctx.author
 
         url = steam_profile_link
 
-        if url == None:
+        if url is None:
             try:
                 msg = await ctx.send("Enter your Steam Profile URL. This message is active for 2 minutes")
                 await ctx.send("To find your profile link, open steam in a browser, and go to your profile. Then copy the link and paste it here.")
-                message = await self.client.wait_for('message', timeout=2*60, check=check)
+                message = await self.client.wait_for('message', timeout=2 * 60, check=check)
 
                 url = message.content.lower()
 
             except asyncio.TimeoutError:
                 await msg.edit('This message is now inactive because you took too long to respond')
-                return 
+                return
 
         _id = await self.get_id_from_url(url=url)
-        if _id == None:
+        if _id is None:
             await ctx.send("I couldn't find your steam profile from that. Try running `help csgo`")
-            return 
+            return
 
         key = os.environ.get('STEAM_API_KEY')
         stats = requests.get(f"http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v2/?appid=730&key={key}&steamid={_id}")
@@ -60,14 +60,15 @@ class CSGOStats(commands.Cog):
             await ctx.send("Uh-oh. Error.\nMaybe a Steam Profile with those details doesn't exist?\nMaybe they don't play CS:GO?\nMaybe their game details are private?")
             return
 
-        if stats.status_code == 500: #Status code is 500 when profile not found or profile private
+        if stats.status_code == 500:  # Status code is 500 when profile not found or profile private
             await ctx.send("I think your profile and/or game details are set to private. Make them public and try again")
-            return 
+            return
 
         stats = stats.json()
-                
-        json = stats['playerstats']
-        gstats = json['stats'] #game stats
+
+        playerstats = stats['playerstats']
+        gstats = playerstats['stats']  # game stats
+
         def get_pos(name):
             for i in range(0, len(gstats)):
                 if gstats[i]['name'] == name:
@@ -80,9 +81,9 @@ class CSGOStats(commands.Cog):
                 value = dict_['value']
                 try:
                     return "{:,}".format(value)
-                except: #If the value is NoneType
+                except TypeError:  # If the value is NoneType
                     return value
-            except:
+            except KeyError:
                 pass
 
         def get_value_without_comma(name):
@@ -91,21 +92,20 @@ class CSGOStats(commands.Cog):
                 dict_ = gstats[pos]
                 value = dict_['value']
                 return value
-            except:
+            except KeyError:
                 pass
-            
 
         kills = (get_value('total_kills'))
         deaths = (get_value('total_deaths'))
 
         _kills = get_value_without_comma('total_kills')
         _deaths = get_value_without_comma('total_deaths')
-        kd = (_kills)/(_deaths)
+        kd = _kills / _deaths
 
         playtime = (get_value_without_comma('total_time_played'))
-        playtime = playtime/3600
+        playtime = playtime / 3600
         playtime = (round(playtime))
-        
+
         games_won = (get_value('total_wins'))
         damage = (get_value('total_damage_done'))
         money = (get_value('total_money_earned'))
@@ -130,44 +130,45 @@ class CSGOStats(commands.Cog):
 """,
             color=random.choice(hex_colors.colors)
         )
-        
+
         await ctx.send(embed=em)
-        
 
     @commands.command(name='csgodetail', aliases=['csgostatsdetail'], help='Get CSGO Stats of a Steam account')
     async def csgo_detail(self, ctx, steam_profile_link=None):
-        def check(message:discord.Message) -> bool:
-            return message.author == ctx.author 
+        def check(message: discord.Message) -> bool:
+            return message.author == ctx.author
 
         url = steam_profile_link
 
-        if url == None:
+        if url is None:
             try:
                 msg = await ctx.send("Enter your Steam Profile URL. This message is active for 2 minutes")
                 await ctx.send("To find your profile link, open steam in a browser, and go to your profile. Then copy the link and paste it here.")
-                message = await self.client.wait_for('message', timeout=2*60, check=check)
+                message = await self.client.wait_for('message', timeout=2 * 60, check=check)
 
                 url = message.content.lower()
 
             except asyncio.TimeoutError:
                 await msg.edit('This message is now inactive because you took too long to respond')
-                return 
+                return
 
         _id = await self.get_id_from_url(url=url)
-        if _id == None:
+        if _id is None:
             await ctx.send("I couldn't find your steam profile from that. Try running `help csgo`")
-            return  
+            return
 
         key = os.environ.get('STEAM_API_KEY')
         stats = requests.get(f"http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v2/?appid=730&key={key}&steamid={_id}")
-        if stats.status_code == 500:
+
+        if stats.status_code == 500:  # Error code 500 is a generic status code sent when the website/API can't pin-point the problem
             await ctx.send("I think your profile and/or game details are set to private. Make them public and try again")
-            return 
+            return
 
         stats = stats.json()
-                
-        json = stats['playerstats']
-        gstats = json['stats'] #game stats
+
+        playerstats = stats['playerstats']
+        gstats = playerstats['stats']  # game stats
+
         def get_pos(name):
             for i in range(0, len(gstats)):
                 if gstats[i]['name'] == name:
@@ -180,21 +181,21 @@ class CSGOStats(commands.Cog):
                 value = dict_['value']
                 try:
                     return "{:,}".format(value)
-                except: #If the value is NoneType
+                except TypeError:  # If the value is NoneType
                     return value
-            except:
+            except KeyError:
                 pass
 
-        #Pistol
+        # Pistol
         glock = (get_value('total_kills_glock'))
         usp = (get_value('total_kills_hkp2000'))
         dual = (get_value('total_kills_elite'))
         p250 = (get_value('total_kills_p250'))
         tec9 = (get_value('total_kills_tec9'))
         fiveseven = (get_value('total_kills_fiveseven'))
-        deag = (get_value('total_kills_deagle'))     
+        deag = (get_value('total_kills_deagle'))
 
-        #Smg
+        # Smg
         mac10 = (get_value('total_kills_mac10'))
         mp9 = (get_value('total_kills_mp9'))
         mp7 = (get_value('total_kills_mp7'))
@@ -202,13 +203,13 @@ class CSGOStats(commands.Cog):
         p90 = (get_value('total_kills_p90'))
         bizon = (get_value('total_kills_bizon'))
 
-        #Shotgun
+        # Shotgun
         nova = (get_value('total_kills_nova'))
         xm1014 = (get_value('total_kills_xm1014'))
         sawed = (get_value('total_kills_sawdoff'))
         mag7 = (get_value('total_kills_mag7'))
 
-        #Rifle
+        # Rifle
         galil = (get_value('total_kills_galilar'))
         famas = (get_value('total_kills_famas'))
         ak47 = (get_value('total_kills_ak47'))
@@ -220,10 +221,10 @@ class CSGOStats(commands.Cog):
         g3sg1 = (get_value('total_kills_g3sg1'))
         scar20 = (get_value('total_kills_scar20'))
 
-        #Misc
+        # Misc
         negev = (get_value('total_kills_negev'))
         m249 = (get_value('total_kills_m249'))
-        zeus = (get_value('total_kills_taser'))        
+        zeus = (get_value('total_kills_taser'))
 
         em = discord.Embed(
             title=f"CSGO Stats for {_id}",
@@ -277,7 +278,6 @@ class CSGOStats(commands.Cog):
             inline=False
         )
         await ctx.send(embed=em)
-
 
 
 def setup(client):
