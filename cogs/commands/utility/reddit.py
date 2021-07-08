5da15787e75d934bab4a5cbf47ecb2e7a79a3d8c
dev-shah-2204 <dgm82382@gmail.com>
2021-07-08 15:32:04 +0530

    Use requests instead of aiohttp

--- cogs/commands/utility/reddit.py
+++ cogs/commands/utility/reddit.py
@@ -1,43 +1,46 @@
 import discord
 import hex_colors
 import random
+import requests
 
 from discord.ext import commands
-from aiohttp import ClientSession
 
 
 class Reddit(commands.Cog):
     def __init__(self, client):
-        self.client = client 
+        self.client = client
 
-    @commands.command(name='reddit', aliases=['subreddit','getredditpost'], help='Gets a post from the subreddit provided')
+    @commands.command(name='reddit', aliases=['subreddit', 'getredditpost'],
+                      help='Gets a post from the subreddit provided')
     @commands.cooldown(1, 3, commands.BucketType.user)
     async def get_reddit_post(self, ctx, *, subreddit):
-        url = f'https://meme-api.herokuapp.com/gimme/{subreddit}' #This api only sends posts with images or gifs.
+        url = f'https://meme-api.herokuapp.com/gimme/{subreddit}'  # This api only sends posts with images or gifs.
 
+        post = requests.get(url=url).json()
+        # Check if the post is nsfw
+        if 'nsfw' in post:  # Sometimes it raises KeyError
+            if post['nsfw']:
+                if not ctx.channel.is_nsfw():
+                    await ctx.send("The post I got from that subreddit is marked NSFW. I cannot send it here")
+                    return
 
-        async with ClientSession() as session:
-            async with session.get(url) as output:
-                post = await output.json()
-                
-                #Check if the post is nsfw
-                if 'nsfw' in post: #Sometimes it raises KeyError
-                    if post['nsfw'] == True:
-                        if not ctx.channel.is_nsfw():
-                            await ctx.send("The post I got from that subreddit is marked NSFW. I cannot send it here")
-                            return
-               
-                try:
-                    image = post['url'] #the image
-                    title = post['title'] #the title of the reddit post
+        try:
+            image = post['url']  # the image
+            title = post['title']  # the title of the reddit post
+            link = post['postLink']  # the link to the post
 
-                    em = discord.Embed(title=title, color=random.choice(hex_colors.colors))
-                    em.set_image(url=post['url'])
-                    em.set_footer(text=f"👍 {post['ups']} | Author: u/{post['author']}") #post['ups'] is the upvotes, post['author'] is the author
+            em = discord.Embed(
+                title=f"[{title}]({link})",
+                color=random.choice(hex_colors.colors)
+            )
+            em.set_image(url=image)
+            em.set_footer(
+                text=f"👍 {post['ups']} | Author: u/{post['author']}")  # post['ups'] is the upvote count, post['author'] is the author
 
-                    await ctx.send(embed=em)
-                except KeyError:
-                    await ctx.send(f"I couldn't find a subreddit called `{subreddit}`")
+            await ctx.send(embed=em)
+        except KeyError:
+            await ctx.send(f"I couldn't find a subreddit called `{subreddit}`")
+
 
 def setup(client):
     client.add_cog(Reddit(client))
