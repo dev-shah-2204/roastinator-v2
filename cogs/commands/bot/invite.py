1eea259d5d843a959aa7b2045df0864ceef1c84e
dev-shah-2204 <dgm82382@gmail.com>
2021-07-19 14:44:32 +0530

    Fix server invite link

--- cogs/commands/bot/invite.py
+++ cogs/commands/bot/invite.py
@@ -11,7 +11,7 @@ class Invite(commands.Cog):
     async def invite(self, ctx):
         em = discord.Embed(
             title="Thank you for inviting me",
-            description="Click [here](https://discord.com/api/oauth2/authorize?client_id=822795444089782293&permissions=2013654102&scope=bot) to add me to your server\nClick [here](discord.gg/GG647gySEy) to join the support server."
+            description="Click [here](https://discord.com/api/oauth2/authorize?client_id=822795444089782293&permissions=2013654102&scope=bot) to add me to your server\nClick [here](https://discord.gg/GG647gySEy) to join the support server."
         )
         em.set_thumbnail(url=self.client.user.avatar_url)
         em.set_footer(text='Bot created by StatTrakDiamondSword#9696 (Tag maybe outdated)')
