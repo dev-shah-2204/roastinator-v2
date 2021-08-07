52b3808f02f5195ecfe1780e4ba0b1163ed9a3b2
dev-shah-2204 <dgm82382@gmail.com>
2021-08-07 07:43:42 +0530

    Use database instead of list

--- cogs/events/ignore_people.py
+++ cogs/events/ignore_people.py
@@ -1,25 +1,39 @@
 import discord 
 
+from db import *
 from discord.ext import commands 
 
 class IgnorePeople(commands.Cog):
     def __init__(self, client):
         self.client = client 
         
-    people_list = []
+    cache = []
+    cache_updated = False
     
     @commands.Cog.listener()
     async def on_message(self, msg):
-        if str(msg.author.id) in self.people_list:
-            return 
+        _id = str(msg.author.id)
+        if self.cache_updated == False:
+            db.execute("SELECT * FROM command_blacklist")
+            db.fetchall()
+            for tup in db:
+                print(tup)
+                self.cache.append(tup[0])
+            self.cache_updated  = True 
+        
+        if _id in self.cache:
+            return
+        
         
     @commands.command(name = 'ignore')
     async def ignore(self, ctx, user_id):
         if ctx.author.id != 416979084099321866:
             return 
         
-        self.people_list.append(user_id)   
-    
+        db.execute(f"INSERT INTO command_blacklist(user_id) VALUES '{user_id}'")    
+        database.commit() 
+        cache.append(str(user_id))   
+        await ctx.send(f"Added {user_id} to the blacklist")
     
         
 def setup(client):
