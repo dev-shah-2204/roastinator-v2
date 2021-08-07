4f5ce16bf1199e19b84b1b5dc006ca7311f2f16b
dev-shah-2204 <dgm82382@gmail.com>
2021-08-07 07:43:15 +0530

    Help me check bugs please

--- cogs/events/auto_mod.py
+++ cogs/events/auto_mod.py
@@ -22,6 +22,7 @@ class AutoModEvent(commands.Cog):
             status = self._status[str(guild)] #Cache
         return status
 
+
     async def get_blacklist(self, guild):
         db.execute(f"SELECT * FROM am_{guild}")
         lst = db.fetchall()
@@ -34,7 +35,9 @@ class AutoModEvent(commands.Cog):
 
         cache[str(guild)] = []
         cache[str(guild)] = blacklist #Adding in cache
+        print(blacklist)
         return blacklist
+
 
     @commands.Cog.listener()
     async def on_message(self, msg):
@@ -42,28 +45,28 @@ class AutoModEvent(commands.Cog):
         if status == 'enabled':
             member = msg.guild.get_member(msg.author.id)
             if member is not None:
-                if not member.guild_permissions.manage_messages or member.guild_permissions.manage_messages: #If message author doesn't have manage_messages permission
+                # if not member.guild_permissions.manage_messages: #If message author doesn't have manage_messages permission
                                        
-                    if str(msg.guild.id) in cache:
-                        blacklist = cache[str(msg.guild.id)]
-                        print(blacklist)
+                if str(msg.guild.id) in cache:
+                    blacklist = cache[str(msg.guild.id)]
+                    print(blacklist)
 
-                    else:
-                        blacklist = await self.get_blacklist(msg.guild.id)
-                        print(blacklist)
-                        cache[str(msg.guild.id)] = blacklist
-                        
-                    for words in blacklist:
-                        print(words)
-                        for word in words:
-                            print(word)
-                            if word.lower() in msg.content.lower():
-                                await msg.delete()
-                                try:
-                                    await msg.author.send(f"Hey! That word is not allowed in {msg.guild.name}")
-                                except:
-                                    pass
-                                break
+                else:
+                    blacklist = await self.get_blacklist(msg.guild.id)
+                    print(blacklist)
+                    cache[str(msg.guild.id)] = blacklist
+                    
+                for words in blacklist:
+                    print(words)
+                    for word in words:
+                        print(word)
+                        if word.lower() in msg.content.lower():
+                            await msg.delete()
+                            try:
+                                await msg.author.send(f"Hey! That word is not allowed in {msg.guild.name}")
+                            except:
+                                pass
+                            break
 
 
 def setup(client):
