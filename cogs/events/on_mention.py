ecd836e35e60ed7e70cccb957efa8c97adcc45be
dev-shah-2204 <dgm82382@gmail.com>
2021-08-10 20:00:42 +0530

    Remove unnecessary code

--- cogs/events/on_mention.py
+++ cogs/events/on_mention.py
@@ -16,14 +16,10 @@ class OnMention(commands.Cog):
         if msg.content == f'<@!{self.client.user.id}>' or msg.content == f'<@{self.client.user.id}>':
             #If the bot is mentioned
             db.execute(f"SELECT prefix FROM Prefix WHERE guild = '{msg.guild.id}'")
-            for row in db:
-                prefix = row
-            
-            prefix = str(prefix)
-            prefix = prefix.replace("('",'') #complex
-            prefix = prefix.replace("',)",'') #very complex mmmhm
+            prefix = db.fetchone()
+
             em = discord.Embed(
-                title=f'My prefix for this server is `{prefix}`',
+                title=f'My prefix for this server is `{prefix[0]}`',  # prefix is a tuple
                 color=random.choice(hex_colors.colors)
             )
             await msg.channel.send(embed=em)
