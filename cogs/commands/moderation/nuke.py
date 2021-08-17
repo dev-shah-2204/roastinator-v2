74adb315c213be884dbd7eb547cdea31aaa0830a
dev-shah-2204 <dgm82382@gmail.com>
2021-08-17 14:28:48 +0530

    Reformat

--- cogs/commands/moderation/nuke.py
+++ cogs/commands/moderation/nuke.py
@@ -3,22 +3,23 @@ import hex_colors
 
 from discord.ext import commands
 
+
 class Nuke(commands.Cog):
     def __init__(self, client):
         self.client = client
+
 
     @commands.command(name='nuke', help='Deletes the channel and makes a copy of it')
     @commands.has_permissions(manage_channels=True)
     @commands.bot_has_permissions(administrator=True)
     async def nuke(self, ctx):
-        existing_channel = ctx.channel
-        pos = existing_channel.position
-        
+        existing_channel = discord.TextChannel(ctx.channel)
+
         new_channel = await existing_channel.clone(reason=f'Original was nuked by {ctx.author}') #Reason to be registered in the audit log
-        await new_channel.edit(position=pos)
+        await new_channel.edit(position=existing_channel.position)
         try:
             await ctx.channel.delete()
-        except:
+        except discord.Forbidden:
             await ctx.send("I couldn't delete the channel, maybe this is a community updates channel?") #Channels that are set for community updates cannot be deleted without transferring the community updates to another channel
 
         em = discord.Embed(
@@ -27,6 +28,7 @@ class Nuke(commands.Cog):
                     color=hex_colors.m_red)
 
         await new_channel.send(embed=em)
+
 
 def setup(client):
     client.add_cog(Nuke(client))
