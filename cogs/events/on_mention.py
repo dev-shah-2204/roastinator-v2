9402edbb254cfa1ed89f4346754e854d3619183c
dev-shah-2204 <dgm82382@gmail.com>
2021-08-24 21:38:02 +0530

    Fix caching issues

--- cogs/events/on_mention.py
+++ cogs/events/on_mention.py
@@ -13,23 +13,24 @@ class OnMention(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, msg):
-        with open('prefix.json', 'r') as f:
-            cache = json.load(f)
-
-        guild = str(msg.guild.id)
-        if guild in cache:
-            prefix = cache[guild]
-        else:
-            db.execute(f"SELECT prefix FROM Prefix WHERE guild = '{guild}'")
-            prefix = db.fetchone()
-            prefix = prefix[0]
-
-            cache[guild] = prefix
-            with open('prefix.json', 'w') as g:
-                g.dump(cache, g)
-
         if msg.content == f'<@!{self.client.user.id}>' or msg.content == f'<@{self.client.user.id}>':
             #If the bot is mentioned
+            with open('prefix.json', 'r') as f:
+                cache = json.load(f)
+
+            guild = str(msg.guild.id)
+
+            if guild in cache:
+                prefix = cache[guild]
+            else:
+                db.execute(f"SELECT prefix FROM Prefix WHERE guild = '{guild}'")
+                prefix = db.fetchone()
+                prefix = prefix[0]
+                cache[str(guild)] = prefix
+
+                with open('prefix.json', 'w') as g:
+                    json.dump(cache, g)
+
             em = discord.Embed(
                 title=f'My prefix for this server is `{prefix}`',  # prefix is a tuple
                 color=random.choice(hex_colors.colors)
