8a58e64b2936024f85f8d8bbf24341d23319b9fc
dev-shah-2204 <dgm82382@gmail.com>
2021-08-26 14:40:03 +0530

    Fix commands

--- cogs/events/ignore_people.py
+++ cogs/events/ignore_people.py
@@ -3,6 +3,7 @@ import discord
 from db import *
 from discord.ext import commands 
 
+
 class IgnorePeople(commands.Cog):
     def __init__(self, client):
         self.client = client 
@@ -13,36 +14,38 @@ class IgnorePeople(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, msg):
         _id = str(msg.author.id)
-        if self.cache_updated == False:
+
+        if not self.cache_updated:
             db.execute("SELECT * FROM command_blacklist")
             db.fetchall()
             for tup in db:
                 print(f"ignore tuple: {tup}")
-                self.cache.append(tup[0])
-            self.cache_updated  = True 
+                self.cache.append(str(tup[0]))
+            self.cache_updated = True
         
-        if _id in self.cache:
+        if str(_id) in self.cache:
             return
         
         
     @commands.command(name='ignore')
-    async def ignore(self, ctx, user_id):
+    async def ignore_people(self, ctx, user_id):
         if ctx.author.id != 416979084099321866:
             return 
         
         db.execute(f"INSERT INTO command_blacklist (user_id) VALUES ('{user_id}')")    
         database.commit() 
-        cache.append(str(user_id))   
+        self.cache.append(str(user_id))
         await ctx.send(f"Added {user_id} to the blacklist")
-        
+
+
     @commands.command(name='unignore')
-    async def ignore(self, ctx, user_id):
+    async def unignore_people(self, ctx, user_id):
         if ctx.author.id != 416979084099321866:
             return
         
         db.execute(f"DELETE FROM command_blacklist WHERE user_id = '{user_id}'")
         database.commit()
-        cache.remove(str(user_id))
+        self.cache.remove(str(user_id))
         await ctx.send(f"Removed {user_id} from the blacklist")
     
         
