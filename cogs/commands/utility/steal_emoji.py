275ee452404290566b0f832bb6ea860628e028cf
dev-shah-2204 <dgm82382@gmail.com>
2021-09-01 14:03:09 +0530

    Reformat, handle more errors

--- cogs/commands/utility/steal_emoji.py
+++ cogs/commands/utility/steal_emoji.py
@@ -1,12 +1,7 @@
-"""
-Got help from
-https://blacktooth-bot.com
-for this one
-"""
-
 import discord
 import requests
 import shutil
+import hex_colors
 
 from discord.ext import commands
 
@@ -18,69 +13,68 @@ class StealEmoji(commands.Cog):
     @commands.command(aliases=["stealemoji", "emojiadd"], description="Download emojis that you have access to and upload them to your own server.")
     @commands.has_permissions(manage_emojis=True)
     async def steal(self, ctx, emoji_name, custom_emoji_name=None):
-        await self.emoji_from_url(ctx, emoji_name, custom_emoji_name)
+        if len(ctx.message.attachments) == 0:
+            image = emoji_name
+            emoji_name = None
+            image_url = None
 
+        elif len(ctx.message.attachments) > 0:
+            image_url = ctx.message.attachments[0].url
 
-    async def emoji_from_url(self, ctx, emoji_name, custom_emoji_name=None, image=None):
-        if image is None:
-            if len(ctx.message.attachments) == 0:
-                image = emoji_name
-                emoji_name = None
-
-        image_url = None
         if image:
             try:
-                image = await discord.ext.commands.PartialEmojiConverter.convert(self, ctx=ctx, argument=image)
+                try:
+                    image = await discord.ext.commands.PartialEmojiConverter.convert(self, ctx=ctx, argument=image)
+                except commands.BadArgument or commands.CommandError as e:
+                    await ctx.send("An error occured while stealing the emoji from that source. Sorry")
+                    return
 
-                if custom_emoji_name is not None:
+                if custom_emoji_name:
                     emoji_name = custom_emoji_name
                 else:
-                    emoji_name = image.name
+                    custom_emoji_name = image.name
 
                 image_url = image.url
 
             except commands.BadArgument:
                 image_url = image
 
-        elif len(ctx.message.attachments) > 0:
-            image_url = ctx.message.attachments[0].url
-
         try:
-            await self.install_emoji(ctx=ctx, emoji_json={"title": emoji_name, "image": image_url})
+            response = requests.get(image_url, stream=True)
+
+            if response.status_code == 200:
+                with open(f"./emojis/{custom_emoji_name}.gif", "wb") as img:
+                    response.raw.decode_content = True
+                    shutil.copyfileobj(response.raw, img)
+
+            else:
+                raise Exception(f"Bad status code uploading {custom_emoji_name} received: {response.status_code}")
+
+            with open(f"./emojis/{custom_emoji_name}.gif", "rb") as image:
+                try:
+                    if isinstance(ctx, discord.Guild):
+                        new_emoji = await ctx.create_custom_emoji(name=custom_emoji_name, image=image.read())
+                    else:
+                        new_emoji = await ctx.message.guild.create_custom_emoji(name=custom_emoji_name, image=image.read())
+
+                        embed = discord.Embed(
+                            title="Emoji added successfully",
+                            colour=hex_colors.l_green,
+                            description=f"`:{custom_emoji_name}:`"
+                        )
+                        embed.set_thumbnail(url=image_url)
+
+                        await ctx.message.channel.send(embed=embed)
+
+                except discord.HTTPException as e:
+                    if e.code == 400:
+                        await ctx.send("Only letters, numbers and underscores are allowed in emoji names.")
+                        return
+
+                except Exception as e:
+                    print(e)
         except requests.exceptions.MissingSchema:
-            await ctx.send(f"{image_url}... That doesn't seem like an emoji or an image")
-
-
-    async def install_emoji(self, ctx, emoji_json):
-        response = requests.get(emoji_json["image"], stream=True)
-
-        if response.status_code == 200:
-            with open(f"./emojis/{emoji_json['title']}.gif", "wb") as img:
-                response.raw.decode_content = True
-                shutil.copyfileobj(response.raw, img)
-
-        else:
-            raise Exception(f"Bad status code uploading {emoji_json['title']} received: {response.status_code}")
-
-        with open(f"./emojis/{emoji_json['title']}.gif", "rb") as image:
-            try:
-                if isinstance(ctx, discord.Guild):
-                    new_emoji = await ctx.create_custom_emoji(name=emoji_json['title'], image=image.read())
-                else:
-                    new_emoji = await ctx.message.guild.create_custom_emoji(name=emoji_json['title'], image=image.read())
-            except discord.HTTPException as e:
-                if e.code == 400:
-                    await ctx.send("Only letters, numbers and underscores are allowed in emoji names.")
-                    return
-
-            embed = discord.Embed(
-                title="Emoji added successfully",
-                colour=discord.Color.green(),
-                description=f"`:{emoji_json['title']}:`"
-                )
-            embed.set_thumbnail(url=emoji_json["image"])
-
-            await ctx.message.channel.send(embed=embed)
+            await ctx.send(f"`{image_url}` doesn't seem like an emoji or an image")
 
 
 def setup(client):
