c2c08c9046e1c4e71469867ebb4a40e4f3cee61b
dev-shah-2204 <dgm82382@gmail.com>
2021-09-05 09:09:15 +0530

    Don't give auto-role to bots

--- cogs/events/on_member_join.py
+++ cogs/events/on_member_join.py
@@ -9,15 +9,18 @@ class MemberJoin(commands.Cog):
     def __init__(self, client):
         self.client = client 
 
+
     async def get_status(self, guild):
         db.execute(f"SELECT _status FROM autorole WHERE guild = '{guild}'")
         status = await get_data(db=db)
         return status 
 
+
     async def get_role(self, guild):
         db.execute(f"SELECT _role FROM autorole WHERE guild = '{guild}'")
         role = await get_data(db=db)
         return role 
+
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
@@ -31,16 +34,17 @@ class MemberJoin(commands.Cog):
         status = await self.get_status(guild.id)
         if status == 'enabled':
             try:
-                await member.add_roles(role)
+                if not member.bot:
+                    await member.add_roles(role)
             except:
                 try:
                     await member.guild.owner.send("You setup the auto-role but either I don't have permissions to Manage Roles or my role is too low. Put my role above the role that you want me to assign to the users.")
                 except:
-                    def check(message:discord.Message) -> bool:
-                        return message.author == user and message.guild == member.guild
+                    def check(message: discord.Message) -> bool:
+                        return message.author == member.guild.owner and message.guild == member.guild
 
                     message = await self.client.wait_for('message', check=check)
-                    await message.channel.send(f"<@!{user.id}>, you setup the auto-role but either I don't have permissions to Manage Roles or my role is too low. Put my role above the role that you want me to assign to the users. Since your DMs are closed, I had to reply this way")
+                    await message.channel.send(f"<@!{member.guild.owner.id}>, you setup the auto-role but either I don't have permissions to Manage Roles or my role is too low. Put my role above the role that you want me to assign to the users. Since your DMs are closed, I had to reply this way")
             
 
 def setup(client):
