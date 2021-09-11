42f246906810fdf78288c5fbbb1b6441c1a2aba3
dev-shah-2204 <dgm82382@gmail.com>
2021-09-11 15:15:26 +0530

    Add attachment in embed

--- cogs/commands/utility/snipe.py
+++ cogs/commands/utility/snipe.py
@@ -20,15 +20,23 @@ class Snipe(commands.Cog):
         del_msg[str(message.channel.id)]['author'] = message.author
         del_msg[str(message.channel.id)]['time'] = datetime.now(pytz.utc) #This will be used for the timestamp
 
+        if len(message.attachments) > 0:
+            del_msg[str(message.channel.id)]['attachment'] = str(message.attachments[0].url)
+
+
     @commands.command(name='snipe', help='Check the last deleted message in the channel')
     async def snipe(self, ctx):
         try:
             msg_content = del_msg[str(ctx.channel.id)]['content']
             if msg_content == '': #If the message had no text, it means that it had an attachment. Since the message is deleted, we can't retrieve that.
-                msg_content = "There was an image or an embed in the deleted message that couldn't be loaded"
+                msg_content = "There was an image or an embed in the deleted message that couldn't be loaded, but here's the url"
+
 
             em = discord.Embed(description=msg_content, color=random.choice(hex_colors.colors), timestamp=del_msg[str(ctx.channel.id)]['time'])
             em.set_author(name=f"{del_msg[str(ctx.channel.id)]['author']} said:", icon_url=del_msg[str(ctx.channel.id)]['author'].avatar_url)
+
+            if del_msg[str(ctx.channel.id)]['attachment'] is not None:
+                em.description = f"{msg_content}\n[**Attachment**]({del_msg[str(ctx.channel.id)]['attachment']})"
 
             await ctx.send(embed=em)
         except:
