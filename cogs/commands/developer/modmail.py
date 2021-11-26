a40aed862a3a56de6a00728a6a4f4d661e1b1104
dev-shah-2204 <dgm82382@gmail.com>
2021-11-26 15:42:40 +0530

    Add confirmation embed for banmodmail and unbanmodmail

--- cogs/commands/developer/modmail.py
+++ cogs/commands/developer/modmail.py
@@ -1,4 +1,5 @@
 import discord
+import hex_colors
 
 from db import *
 from discord.ext import commands
@@ -11,7 +12,7 @@ class ModMail(commands.Cog):
     
     # The banned people will be unbanned when you restart the bot. You need to setup a database. I am unable to do so on heroku for some reason.
     @commands.command(name='banmodmail', aliases=['banmm'], help="This is a developer command")
-    async def banmodmail(self, ctx, user:discord.User):
+    async def banmodmail(self, ctx, user:discord.User, *, reason):
         if ctx.author.id != self.client.owner_id:
             await ctx.send("You can't do that")
             return
@@ -22,9 +23,20 @@ class ModMail(commands.Cog):
             await ctx.send(f"Won't take mod-mail from {user} now")
             banned_people.append(user.id)
 
+            em = discord.Embed(title="Un-banned from ModMail",
+                               description=f"**Reason**: {reason}",
+                               color=hex_colors.m_red)
+            em.set_footer(text="Action taken by StatTrakDiamondSword#5493")
+            try:
+                user.send(embed=em)
+            except:
+                await ctx.send("User has closed DMs")
+
+
+
     # You might want to unban them later too
     @commands.command(name='unbanmodmail', aliases=['unbanmm'], help="This is a developer command")
-    async def unbanmodmail(self, ctx, user:discord.User):
+    async def unbanmodmail(self, ctx, user:discord.User, reason):
         if ctx.author.id != self.client.owner_id:
             await ctx.send("You can't do that")
             return
@@ -34,6 +46,15 @@ class ModMail(commands.Cog):
             database.commit()
             await ctx.send(f"Unbanned {user} from the mod-mail blacklist")
             banned_people.remove(user.id)
+
+            em = discord.Embed(title="Banned from ModMail",
+                               description=f"**Reason**: {reason}",
+                               color=hex_colors.m_red)
+            em.set_footer(text="Action taken by StatTrakDiamondSword#5493")
+            try:
+                user.send(embed=em)
+            except:
+                await ctx.send("User has closed DMs")
 
 
 def setup(client):
